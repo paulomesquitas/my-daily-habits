8c5c2d2cdf4bf2f95fbc49338c085943734b7c37
Paulo <paulo@mail.com>
2026-03-11 19:14:06 -0400

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <div>
+      <h1>My Daily Habits</h1>
+      <p>Gerencie seus hábitos diários de forma simples e visual.</p>
+
+      <br />
+      <Footer />
+    </div>
+  )
+}
+export default App
---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <footer>
+            <small>Módulo 04 – Desenvolvimento Front-end com React – ITEAM</small>
+        </footer>
+    )
+}
+
+export default Footer
